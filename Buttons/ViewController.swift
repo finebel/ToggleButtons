f29be63e177ccf152e1a7688f249b27a7d21a776
finebel <finebel.fl@gmail.com>
2020-08-21 14:30:05 +0200

    Added .gitigonre file

--- Buttons/ViewController.swift
+++ Buttons/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var collectionOfButtons: Array<UIButton>?
+    
+    private let selectionColor = UIColor.orange
+    private let nonSelectedColor = UIColor.green
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressToggle(_ sender: UIButton) {
+        guard let buttons = collectionOfButtons else { return }
+        
+        for btn in buttons {
+            if btn.tag == sender.tag {
+                btn.backgroundColor = selectionColor
+            } else {
+                btn.backgroundColor = nonSelectedColor
+            }
+        }
+    }
+    
 }
 
